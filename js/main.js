430139d57a8a1e7af3767e71e14854296dc45d54
Ivy <yunjilim@gmail.com>
2019-02-19 11:23:03 -0800

    update text content fixes #1

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@ import toggleActive from './toggleActive.js';
 // select edit buttons
 // add event listeners (click) to each button
 document.querySelectorAll('.edit-button, .save-button').forEach(button => button.addEventListener('click', toggleActive));
+
+// listen for 'input' events from editor
+document.querySelectorAll('.editor').forEach(editor => editor.addEventListener('input', handleEditorInput))
+
+// update text of sibling '.text-element'
+function handleEditorInput(event) {
+  if (event.target.type === 'text' || event.target.tagName === 'TEXTAREA') {
+    const textElement = event.currentTarget.parentNode.querySelector('.text-element');
+    const text = event.target.value;
+    textElement.innerText= text;
+  }
+}
